1eaea7788d0f33dfd0e0a27960b9ac5f64aad5c1
root <account-login@users.noreply.github.com>
2017-04-06 13:52:54 +0800

    add --logfile option.

--- plsmake/__main__.py
+++ plsmake/__main__.py
@@ -4,7 +4,6 @@ from plsmake.app import create_init_env, load_file, resolve, execute
 from plsmake.log import config_logger
 
 
-# TODO: --log
 # TODO: --list-deps
 # TODO: --dry-run
 # TODO: -j
@@ -13,16 +12,17 @@ from plsmake.log import config_logger
 
 def parse_args():
     parser = argparse.ArgumentParser()
-    parser.add_argument('-f', '--file', default='Plsmakefile.py')
-    parser.add_argument('-v', '--verbose', action='count', default=0)
-    parser.add_argument('targets', nargs='+')
+    parser.add_argument('-f', '--file', default='Plsmakefile.py', help='build scripts')
+    parser.add_argument('-v', '--verbose', action='count', default=0, help='increase verbosity')
+    parser.add_argument('--logfile', help='write log to file')
+    parser.add_argument('targets', nargs='+', help='target to build')
 
     return parser.parse_args()
 
 
 def main():
     option = parse_args()
-    config_logger(verbose=option.verbose)
+    config_logger(verbose=option.verbose, logfile=option.logfile)
 
     rule_list, env = load_file(option.file, create_init_env())
     for target in option.targets:
--- plsmake/log.py
+++ plsmake/log.py
@@ -1,5 +1,8 @@
+import json
 import logging
+import sys
 import time
+import traceback
 
 from structlog import configure, DropEvent
 from structlog.stdlib import add_log_level
@@ -29,6 +32,54 @@ def add_timestamp(logger, name, event_dict):
     return event_dict
 
 
+class LogDispatcher:
+    def __init__(self):
+        self.handlers = set()
+
+    def add_handler(self, handler):
+        self.handlers.add(handler)
+
+    def remove_handler(self, handler):
+        self.handlers.remove(handler)
+
+    def __call__(self, logger, name, event_dict):
+        for handler in self.handlers:
+            try:
+                handler(logger, name, event_dict)
+            except Exception:
+                print('exception thrown by hander %r' % (handler,), file=sys.stderr)
+                traceback.print_exc()
+
+        return event_dict
+
+
+class LogWriter:
+    def __init__(self, filename):
+        self.filename = filename
+        self._fp = open(self.filename, 'at')
+
+    def __call__(self, logger, name, event_dict):
+        string = json.dumps(event_dict, default=_json_fallback)
+        self._fp.write(string + '\n')
+
+    def __del__(self):
+        self._fp.close()
+
+
+# from structlog/processors.py
+def _json_fallback(obj):
+    from structlog.threadlocal import _ThreadLocalDictWrapper
+    if isinstance(obj, _ThreadLocalDictWrapper):
+        return obj._dict
+    else:
+        try:
+            serializer = obj.__json__
+        except AttributeError:
+            return repr(obj)
+        else:
+            return serializer()
+
+
 class LogRenderer:
     def __init__(self, level=logging.INFO):
         self.level = level
@@ -48,7 +99,10 @@ class LogRenderer:
         return '{name:9s}{event}: {pairs}'.format_map(locals())
 
 
-def config_logger(verbose=0):
+_LOG_DISPATCHER = LogDispatcher()
+
+
+def config_logger(verbose=0, logfile=None):
     if verbose >= 2:
         level = logging.DEBUG
     elif verbose >= 1:
@@ -56,11 +110,15 @@ def config_logger(verbose=0):
     else:
         level = logging.WARNING
 
+    if logfile is not None:
+        _LOG_DISPATCHER.add_handler(LogWriter(logfile))
+
     processors = [
         add_log_level,
         add_timestamp,
         format_exc_info,
         StackInfoRenderer(),
+        _LOG_DISPATCHER,
         LogRenderer(level=level),
     ]
     configure(processors=processors)
